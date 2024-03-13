98c7df215785ae9b9bb4d434f2423f7b2ed0c464
AnBrnd <anais.mauricette.bernard@gmail.com>
2024-03-13 12:27:36 +0100

    Produits tout ok

--- src/main/java/fr/epsi/msprsansjdbc/controller/ProduitController.java
+++ src/main/java/fr/epsi/msprsansjdbc/controller/ProduitController.java
@@ -3,15 +3,10 @@ package fr.epsi.msprsansjdbc.controller;
 import fr.epsi.msprsansjdbc.entities.Produit;
 import fr.epsi.msprsansjdbc.service.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Controller
